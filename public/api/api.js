b61f62db8a9a484850bdd9ccf124fcaa8a157bff
Jake-Holliman <88987855+Jake-Holliman@users.noreply.github.com>
2022-06-06 17:54:51 -0500

    Update api.js

--- public/api/api.js
+++ public/api/api.js
@@ -1,183 +1,318 @@
 
-function create() {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    if (name != null && email != null && password != null && name != '' && email != '' && password != '') {
-        const Http = new XMLHttpRequest();
-        var url = '/account/create/' + name + '/' + email + '/' + password;
-        Http.open("GET", url);
-        Http.send();
-        Http.onreadystatechange = (e) => {
-            if (Http.responseText != null && Http.responseText != '') {
-                if (Http.status == 200) {
-                    document.getElementById('output').value = "Account was created successful.";
+function Create() 
+{
+    var name     =  document.getElementById('name').value;
+    var email    =  document.getElementById('email').value;
+    var password =  document.getElementById('password').value;
+    var status   =  document.getElementById ('createstatus');
+    var url      =  '/account/Create/' + name + '/' + email + '/' + password;
+ 
+ if (name != null && email != null && password != null && name != '' && email != '' && password != '') 
+   
+     {
+        superagent
+            .get (url)
+            .end (function (err, res)
+            {
+                if (err) 
+                {
+                    console.log (err);
+                    status.innerHTML ='Request Failed!';
+                    setTimeout(function(){status.innerHTML ='';},3000);
                 }
-                else {
-                    document.getElementById('output').value = "Account with same email already exists.";
+                else
+                {
+                    if ((res.text) !=null && (res.text) != '')
+                    {
+                        console.log(res.text);
+                        status.innerHTML ='Account Created!';
+                        setTimeout(function(){status.innerHTML ='';},3000);    
+                    }
+                    else
+                    {
+                        console.log('Account Already Exists!');
+                        status.innerHTML ='Account Already Exists!';
+                        setTimeout(function(){status.innerHTML ='';},3000);
+
+                    }
                 }
-            }
-            else {
-                document.getElementById('output').value = "Create account request failed"
-            }
-        }
+
+            });
     }
-    else {
-        document.getElementById('output').value = "Name, email and password are required";
+ else 
+    {
+        status.innerHTML = 'Account Details are required!';
+
+    }
+            
+}
+
+function Login() 
+{
+    var email    = document.getElementById('loginEmail').value;
+    var password = document.getElementById('loginPassword').value;
+    var status   = document.getElementById ('loginstatus');
+    var url      = '/account/Login/'+ email + '/' + password;
+
+if (email != null && password != null && email != '' && password != '')
+    {
+        superagent
+        .get (url)
+        .end (function (err, res)
+        {
+            if (err) 
+                {
+                    console.log (err);
+                }
+            else
+            {
+                if (res.body)
+                {
+                    console.log(res.body);
+                    status.innerHTML ='Login Successful!';
+                    setTimeout(function(){status.innerHTML ='';},3000);
+
+                }
+                else
+                {
+                    console.log ('Login Failed');
+                    status.innerHTML ='Login Failed!';
+                    setTimeout(function(){status.innerHTML ='';},3000);
+                }
+                
+               
+            }
+
+        });
+    }
+ else 
+    {
+        status.innerHTML = 'Login and Password Field are required!';
+        setTimeout(function(){status.innerHTML ='';},3000);
+    }
+}
+
+function Deposit() 
+{
+    var email   = document.getElementById('depositEmail').value;
+    var amount  = document.getElementById('depositAmount').value;
+    var status  = document.getElementById('depositstatus');
+    var url     = '/account/Deposit/'+ email + '/' + amount;
+
+if (email != null && amount != null && email != '' && amount != '' && amount > 0)
+{
+
+    superagent
+        .get (url)
+        .end (function (err, res)
+        {
+            if (err) 
+            {
+                console.log (err);
+                status.innerHTML ='Request Failed!';
+            }
+            else
+            {
+                if (res.body)
+                {
+
+                    console.log (res.body);
+                    status.innerHTML = amount;
+                    setTimeout(function(){status.innerHTML='';},3000);   
+
+                }
+                else
+                {
+
+                     console.log ('Deposit Failed!');
+                     status.innerHTML ='Deposit Failed!';
+                     setTimeout(function(){status.innerHTML='';},3000);
+               
+                }
+            }
+
+        });
+}
+else 
+    {
+        status.innerHTML = 'Email and Amount Fields are required';
+        setTimeout(function(){status.innerHTML ='';},3000);
     }
 
 }
 
-function login() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    if (email != null && password != null && email != '' && password != '') {
-        const Http = new XMLHttpRequest();
-        var url = '/account/login/' + email + '/' + password;
-        Http.open("GET", url);
-        Http.send();
-        Http.onreadystatechange = (e) => {
-            if (Http.responseText != null && Http.responseText != '') {
-                if (Http.status == 200) {
-                    document.getElementById('output').value = "Login was successful. Your account:\n " + Http.responseText;
-                }
-                else {
-                    document.getElementById('output').value = "The email you entered was not found";
-                }
-            }
-            else {
-                document.getElementById('output').value = "Login request failed or password was not correct"
-            }
-        }
-    }
-    else {
-        document.getElementById('output').value = "Email and password are required";
-    }
-}
+function Withdraw() 
+{
+    var email   = document.getElementById('withdrawEmail').value;
+    var amount  = document.getElementById('withdrawAmount').value;
+    var status  = document.getElementById('withdrawstatus');
+    var url     = '/account/Withdraw/'+ email + '/' + amount;
 
-function deposit() {
-    var email = document.getElementById('email').value;
-    var amount = document.getElementById('amount').value;
-    if (email != null && amount != null && email != '' && amount != '' && amount > 0) {
-        const Http = new XMLHttpRequest();
-        var url = '/account/deposit/' + email + '/' + amount;
-        Http.open("GET", url);
-        Http.send();
-        Http.onreadystatechange = (e) => {
-            if (Http.responseText != null && Http.responseText != '') {
-                if (Http.status == 200) {
-                    document.getElementById('output').value = "Deposit was successful";
-                }
-                else {
-                    document.getElementById('output').value = "The email you entered was not found";
-                }
-            }
-            else {
-                document.getElementById('output').value = "Deposit request failed "
-            }
-        }
-    }
-    else {
-        document.getElementById('output').value = "Email and amount are required and amount needs to be bigger than 0";
-    }
-}
+ if (email != null && amount != null && email != '' && amount != '' && amount > 0)
+    {
 
-function withdraw() {
-    var email = document.getElementById('email').value;
-    var amount = document.getElementById('amount').value;
-    if (email != null && amount != null && email != '' && amount != '' && amount > 0) {
-        const Http = new XMLHttpRequest();
-        var url = '/account/withdraw/' + email + '/' + amount;
-        Http.open("GET", url);
-        Http.send();
-        Http.onreadystatechange = (e) => {
-            if (Http.responseText != null && Http.responseText != '') {
-                if (Http.status == 200) {
-                    document.getElementById('output').value = "Withdraw was successful";
-                }
-                else {
-                    document.getElementById('output').value = "The email you entered was not found";
-                }
+        superagent
+        .get (url)
+        .end (function (err, res)
+        {
+            if (err) 
+            {
+                console.log (err);
+                status.innerHTML ='Request Failed!';
+                setTimeout(function(){status.innerHTML='';},3000); 
             }
-            else {
-                document.getElementById('output').value = "Withdraw request failed "
-            }
-        }
-    }
-    else {
-        document.getElementById('output').value = "Email and amount are required and amount needs to be bigger than 0";
-    }
-}
+            else
+            {
+                if (res.body)
+                {
 
-function transactions() {
-    var email = document.getElementById('email').value;
-    if (email != null && email != '') { //check if email is not empty
-        const Http = new XMLHttpRequest();
-        var url = '/account/transactions/' + email;
-        Http.open("GET", url);
-        Http.send();
-        Http.onreadystatechange = (e) => {
-            if (Http.responseText != null && Http.responseText != '') {
-                if (Http.status == 200) {
-                    document.getElementById('output').value = Http.responseText;
+                    console.log (res.body);
+                    status.innerHTML = amount;
+                    setTimeout(function(){status.innerHTML='';},3000); 
+
                 }
-                else {
-                    document.getElementById('output').value = "The email you entered was not found";
+                else
+                {
+
+                     console.log ('Withdraw Failed or Insufficient Balance!');
+                     status.innerHTML ='Withdraw Failed or Insufficient Balance!';
+                     setTimeout(function(){status.innerHTML='';},3000);
+               
                 }
+            
             }
-            else { //empty server response
-                document.getElementById('output').value = "Transaction request failed "
-            }
-        }
+        });
     }
-    else {
-        document.getElementById('output').value = "Email is required";
+
+    else
+    {
+        status.innerHTML = 'Email and Amount Field is required!';
+        setTimeout(function(){status.innerHTML ='';},3000);
     }
+
 }
 
 
-function balance() {
-    var email = document.getElementById('email').value;
-    if (email != null && email != '') {
-        const Http = new XMLHttpRequest();
-        var url = '/account/balance/' + email;
-        Http.open("GET", url);
-        Http.send();
-        Http.onreadystatechange = (e) => {
-            if (Http.responseText != null && Http.responseText != '') {
-                if (Http.status == 200) {
-                    document.getElementById('output').value = Http.responseText;
-                }
-                else {
-                    document.getElementById('output').value = "The email you entered was not found";
-                }
+function Transactions() 
+{
+    var email   = document.getElementById('transactionEmail').value;
+    var status  = document.getElementById('transactionstatus');
+    var url     = '/account/Transactions/'+ email;
+
+if (email != null && email != '')
+ {
+
+    superagent
+        .get (url)
+        .end (function (err, res)
+        {
+            if (err) 
+            {
+                console.log (err);
             }
-            else {
-                document.getElementById('output').value = "Balance request failed "
+            else
+            {
+                if (res.body)
+                {
+                    console.log (res.body);
+                    status.innerHTML = 'Transaction Sucessful';   
+                    setTimeout(function(){status.innerHTML='';},3000);
+                }
+                else
+                {
+
+                    console.log ('Transaction Failed!');
+                    status.innerHTML ='Transaction Failed!';
+                    setTimeout(function(){status.innerHTML='';},3000);
+               
+                }        
+
             }
-        }
+             
+        });
+ }
+ else
+    {
+        status.innerHTML = "Email Field is required!";
+        setTimeout(function(){status.innerHTML ='';},3000);
     }
-    else {
-        document.getElementById('output').value = "Email is required";
-    }
+
 }
 
-function allData() {
-    const Http = new XMLHttpRequest();
-    var url = '/account/all';
-    Http.open("GET", url);
-    Http.send();
-    Http.onreadystatechange = (e) => {
-        if (Http.responseText != null && Http.responseText != '') {
-            if (Http.status == 200) {
-                document.getElementById('output').value = Http.responseText;
+function Balance() 
+{
+    var email   = document.getElementById('balanceEmail').value;
+    var status  = document.getElementById('balancestatus');
+    var url     = '/account/Balance/'+ email;
+ 
+ if (email != null && email != '') 
+ {
+    superagent
+        .get (url)
+        .end (function (err, res)
+        {
+            if (err) 
+            {
+                console.log (err);
             }
-            else {
-                document.getElementById('output').value = "No data was found";
+            else
+            {
+                if (res.text)
+                {
+                    console.log (res.text);
+                    status.innerHTML = res.text;
+                    setTimeout(function(){status.innerHTML='';},3000);   
+                }
+                else
+                {
+                    console.log ('Account Not Found!');
+                    status.innerHTML ='Account Not Found!';
+                    setTimeout(function(){status.innerHTML='';},3000);
+                } 
+
             }
-        }
-        else { //empty server response
-            document.getElementById('output').value = "All accounts request failed "
-        }
+             
+        });
+                     
+ }
+ else
+    {
+        status.innerHTML = "Email Field is required!";
+        setTimeout(function(){status.innerHTML ='';},3000);
     }
+
+}
+
+
+function AllData() 
+{   
+     var status   =  document.getElementById ('AllDatastatus');
+     var url    = '/account/AllData';
+     superagent
+        .get (url)
+        .end (function (err, res)
+        {
+            if (err) 
+            {
+                console.log (err);
+            }
+            else
+            {
+                if (res.body)
+                {
+                    console.log (res.body);
+                    status.innerHTML = JSON.stringify(res.body);
+                   
+                }
+                else
+                {
+                    console.log ('No Data!');
+                    status.innerHTML ='No Data!';
+                   
+                } 
+
+            }
+             
+        });
+
 }
