96aa5245929a774ab7cad89df1419be26163ee5f
Jake-Holliman <88987855+Jake-Holliman@users.noreply.github.com>
2022-06-09 07:59:49 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function Spa() {
+    return (
+      // <NavBar/>
+      <HashRouter>
+        <div>
+          <NavBar/>        
+          <UserContext.Provider value={{users:[{name:'Jake',email:'jakegholliman@yahoo.com',password:'password',balance:1000}]}}>
+            <div className="container" style={{padding: "20px"}}>
+              <Route path="/" exact component={Home} />
+              <Route path="/CreateAccount/" component={CreateAccount} />
+              <Route path="/login/" component={Login} />
+              <Route path="/deposit/" component={Deposit} />
+              <Route path="/withdraw/" component={Withdraw} />
+              {/* <Route path="/transactions/" component={Transactions} /> */}
+              <Route path="/balance/" component={Balance} />
+              <Route path="/alldata/" component={AllData} />
+            </div>
+          </UserContext.Provider>
+        </div>
+      </HashRouter>
+    );
+  }
+  
+  ReactDOM.render(
+    <Spa/>,
+    document.getElementById('root')
+  );
